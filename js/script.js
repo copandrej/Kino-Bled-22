ab6bae30634092430b07a245ac47439a58666c03
copandrej <ac8560@student.uni-lj.si>
2022-07-23 16:07:47 +0200

    loc/json testing

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
+let temp = null;
 function zapri(el) {
-    (el.clientHeight >= 45) ? el.style.height = "41px" : el.style.height = (el.childNodes[3].clientHeight + 41).toString() + "px";
+    if (temp != null) {
+        let buff = temp;
+        temp = null;
+        zapri(buff);
+    }
+    if (el.clientHeight >= 45) {
+        el.style.height = "41px";
+        temp = null;
+    }
+    else {
+        el.style.height = (el.childNodes[3].clientHeight + 41).toString() + "px";
+        temp = el;
+    }
+
 }
 
 function poravnaj(el) {
@@ -19,11 +33,13 @@ window.addEventListener("load", () => {
     const dnevi = document.getElementsByClassName("skri");
 
     // Dnevi v tednu + jezik browserja
-    const DneviSlo = ["TOREK", "SREDA", "&#268ETRTEK", "PETEK", "SOBOTA"];
-    const DneviEng = ["TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"];
+    const DneviSlo = ["PONEDELJEK", "TOREK", "SREDA", "&#268ETRTEK", "PETEK", "SOBOTA", "NEDELJA"];
+    const DneviEng = ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"];
     const userLang = navigator.language || navigator.userLanguage;
 
-    let trenutniJezik = ""
+    let trenutniJezik = "";
+    let trenutnaLokacija = "park";
+    $(".park").css("color", "#df6437");
 
     if (userLang == "sl" && trenutniJezik != "sl")
         spremembaJezika("sl");
@@ -35,6 +51,28 @@ window.addEventListener("load", () => {
     $(".eng").click(() => { spremembaJezika("en"); });
 
 
+    //gumb za lokacijo
+
+    $(".fes").click(() => {
+        resetColor();
+        $(".fes").css("color", "#df6437");
+        vsebina(trenutniJezik, "fes"); 
+    });
+    $(".itc").click(() => { 
+        resetColor();
+        $(".itc").css("color", "#df6437");
+        vsebina(trenutniJezik, "itc"); 
+    });
+    $(".park").click(() => { 
+        resetColor();
+        $(".park").css("color", "#df6437");
+        vsebina(trenutniJezik, "park"); });
+
+    function resetColor() {
+        $(".fes").css("color", "#ffebc6");
+        $(".itc").css("color", "#ffebc6");
+        $(".park").css("color", "#ffebc6");
+    }
     /**
      * 
      * najprej spemeni dneve v tednu, da je to čim hitreje nato vsebina() spremeni še program
@@ -43,13 +81,12 @@ window.addEventListener("load", () => {
         if (Jezik == "sl") {
             trenutniJezik = "sl";
             //zgoraj opisan hrošč "debug"
-            debug.style.display = "none";         
+            debug.style.display = "none";
             for (let i = 0; i < spremeni.length; i++)
-                spremeni[i].innerHTML = DneviSlo[i];       
+                spremeni[i].innerHTML = DneviSlo[i];
             debug.style.display = "inline";
-            prevedi[0].innerHTML = "ZAVOD ASPEKT    PREDSTAVLJA";
-            prevedi[1].innerHTML = "VSTOP PROST";
-            prevedi[2].innerHTML = "PI&#352ITE NAM";
+            prevedi[0].innerHTML = "KUPI VSTOPNICE";
+            prevedi[1].innerHTML = "PI&#352ITE NAM";
             vsebina(trenutniJezik);
 
         } else {
@@ -58,9 +95,8 @@ window.addEventListener("load", () => {
             for (let i = 0; i < spremeni.length; i++)
                 spremeni[i].innerHTML = DneviEng[i];
             debug.style.display = "inline";
-            prevedi[0].innerHTML = "ASPEKT INSTITUTE    PRESENTS";
-            prevedi[1].innerHTML = "FREE ENTRY";
-            prevedi[2].innerHTML = "EMAIL US";
+            prevedi[0].innerHTML = "BUY TICKETS";
+            prevedi[1].innerHTML = "EMAIL US";
             vsebina(trenutniJezik);
         }
     }
@@ -70,8 +106,8 @@ window.addEventListener("load", () => {
      * js prebere in zgradi html strukturo za dinamično veliko vsebino
      * prostor za izbolšavo z api ali bazo
      */
-    function vsebina(jezik) {
-        let imeDatoteke = `vsebina/${jezik}.json`
+    function vsebina(jezik, lokacija) {
+        let imeDatoteke = `./vsebina/${jezik}.json`
         $.getJSON(imeDatoteke, (data) => {
             for (let i = 0; i < dnevi.length; i++) {
                 let string = "";
